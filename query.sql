83f0f74f84de80532c06b4e328236f53b7088de7
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-12-15 16:53:15 +0530

    order by

--- query.sql
+++ query.sql
@@ -17,3 +17,11 @@ SELECT name, phone, email, list, numberrange FROM mytable WHERE list = 1 AND num
 -- select specific columns and rows with OR where condition
 
 SELECT name, phone, email, list, numberrange FROM mytable WHERE list = 1 OR numberrange = 3;
+
+-- select columns with decending order of id
+
+SELECT * FROM mytable ORDER BY id DESC;
+
+-- select names in ascending alphbetical order 
+
+SELECT id, name FROM mytable ORDER BY name ASC;
