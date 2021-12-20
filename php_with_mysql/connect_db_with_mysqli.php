90d495cf095166ac7ed9729d3d3733c7930c2980
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-12-20 17:00:59 +0530

    updated the source code for mysqli

--- php_with_mysql/connect_db_with_mysqli.php
+++ php_with_mysql/connect_db_with_mysqli.php
@@ -5,11 +5,17 @@
         $databaseUser = "root";
         $databasePass = "";
         $databaseName = "edunet";
-        $databaseConnection = new mysqli($databaseHost, $databaseUser, $databasePass, $databaseName) or die("Connect failed: %s\n". $databaseConnection -> error);
+        $databaseConnection = new mysqli($databaseHost, $databaseUser, $databasePass, $databaseName) or die("Connect failed: %s\n". $databaseConnection->error);
         return $databaseConnection;
     }
     function closeConnection($databaseConnection)
     {
         $databaseConnection->close();
     }
+
+    // check the connection
+
+    $connectionObject = openConnection();
+
+    print_r($connectionObject);
 ?>
