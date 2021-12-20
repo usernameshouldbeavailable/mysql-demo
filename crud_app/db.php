1dd5fd135d28972d947d42e10290b9f7a05df9b0
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-12-20 17:31:36 +0530

    added db.php file to connect to our database

---
+++ crud_app/db.php
@@ -0,0 +1,7 @@
+<?php
+    $databaseHost = "localhost"; // facebook.com micorsoft.com
+    $databaseName = "crud_db";
+    $databaseUser = 'root';
+    $databasePass = '';
+    $mysqli = mysqli_connect($databaseHost, $databaseUser,$databasePass, $databaseName);
+?>
