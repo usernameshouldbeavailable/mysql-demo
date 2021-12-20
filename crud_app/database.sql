6c025f68e661772578edab39d58a2902493170bf
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-12-20 17:31:13 +0530

    creating database and table

--- crud_app/database.sql
+++ crud_app/database.sql
@@ -1,8 +1,12 @@
 /* Create Database and Table */
+
+    -- 1. create database
     create database crud_db;
 
+    -- 2. select your database
     use crud_db;
 
+    -- 3. create users table
     CREATE TABLE `users` (
         `id` int(11) NOT NULL auto_increment,
         `name` varchar(100),
