ff32cb3b92a8dbb28a2588301c370af21c49969a
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-12-21 17:09:53 +0530

    added update query in basics file

--- crud_app/basics.php
+++ crud_app/basics.php
@@ -4,11 +4,17 @@
 
     // insert data
 
-    $query = "INSERT INTO `users`(`name`, `email`, `mobile`) 
-        VALUES ('Avinash Seth', 'avinash@edunetfoundation.org', '1234567890')";
+    // $query = "INSERT INTO `users`(`name`, `email`, `mobile`) 
+        // VALUES ('Avinash Seth', 'avinash@edunetfoundation.org', '1234567890')";
 
-    mysqli_query($mysqli, $query);
+    // mysqli_query($mysqli, $query);
 
-    // $result = mysqli_query($mysqli, "INSERT INTO users(name,email,mobile) VALUES('$name','$email','$mobile')");
+    // update individual row
+
+    // UPDATE users SET mobile = '1472583690' WHERE id = 5;
+
+    $updateQuery = "UPDATE users SET mobile = '1472583690' WHERE id = 5";
+
+    mysqli_query($mysqli, $updateQuery);
 
 ?>
