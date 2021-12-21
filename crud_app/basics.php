80131cdb2ae020cf9430ba22a2db29b8218ba92a
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-12-21 17:26:19 +0530

    delete the query

--- crud_app/basics.php
+++ crud_app/basics.php
@@ -4,27 +4,33 @@
 
     // 1. insert data
 
-    // $query = "INSERT INTO `users`(`name`, `email`, `mobile`) 
-        // VALUES ('Avinash Seth', 'avinash@edunetfoundation.org', '1234567890')";
+    $query = "INSERT INTO `users`(`name`, `email`, `mobile`) 
+        VALUES ('Avinash Seth', 'avinash@edunetfoundation.org', '1234567890')";
 
-    // mysqli_query($mysqli, $query);
+    mysqli_query($mysqli, $query);
 
     // 2. update individual row
 
     // UPDATE users SET mobile = '1472583690' WHERE id = 5;
 
-    // $updateQuery = "UPDATE users SET mobile = '1472583690' WHERE id = 5";
+    $updateQuery = "UPDATE users SET mobile = '1472583690' WHERE id = 5";
 
-    // mysqli_query($mysqli, $updateQuery);
+    mysqli_query($mysqli, $updateQuery);
 
     // 3. select individual user
 
-    // $selectQuery = "SELECT * FROM users WHERE id = 5";
+    $selectQuery = "SELECT * FROM users WHERE id = 5";
 
-    // $queryResult = mysqli_query($mysqli, $selectQuery);
+    $queryResult = mysqli_query($mysqli, $selectQuery);
 
-    // $resultArray = mysqli_fetch_array($queryResult);
+    $resultArray = mysqli_fetch_array($queryResult);
 
-    // echo $resultArray['name'];
+    echo $resultArray['name'];
+
+    // 4. Delete data
+
+    $deleteQuery = "DELETE FROM users WHERE id = 5";
+
+    mysqli_query($mysqli, $deleteQuery);
 
 ?>
