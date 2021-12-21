bef01bf982e990d691d634c0a8ca3316e6586727
usernameshouldbeavailable <avinash@edunetfoundation.org>
2021-12-21 17:16:13 +0530

    fetch one single user

--- crud_app/basics.php
+++ crud_app/basics.php
@@ -2,19 +2,29 @@
 
     include_once('db.php');
 
-    // insert data
+    // 1. insert data
 
     // $query = "INSERT INTO `users`(`name`, `email`, `mobile`) 
         // VALUES ('Avinash Seth', 'avinash@edunetfoundation.org', '1234567890')";
 
     // mysqli_query($mysqli, $query);
 
-    // update individual row
+    // 2. update individual row
 
     // UPDATE users SET mobile = '1472583690' WHERE id = 5;
 
-    $updateQuery = "UPDATE users SET mobile = '1472583690' WHERE id = 5";
+    // $updateQuery = "UPDATE users SET mobile = '1472583690' WHERE id = 5";
 
-    mysqli_query($mysqli, $updateQuery);
+    // mysqli_query($mysqli, $updateQuery);
+
+    // 3. select individual user
+
+    // $selectQuery = "SELECT * FROM users WHERE id = 5";
+
+    // $queryResult = mysqli_query($mysqli, $selectQuery);
+
+    // $resultArray = mysqli_fetch_array($queryResult);
+
+    // echo $resultArray['name'];
 
 ?>
